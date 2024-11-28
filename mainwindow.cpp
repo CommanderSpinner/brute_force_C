c98ed7fa9934f9f441c60c8b2809bddc40a7f18e
CommanderSpinner <klaus.kutulu@gmail.com>
2024-11-28 23:16:36 +0100

    made window not resisable and added title to window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setWindowTitle("Brute Forcer");
+    this->setFixedSize(this->size());
 }
 
 MainWindow::~MainWindow()
