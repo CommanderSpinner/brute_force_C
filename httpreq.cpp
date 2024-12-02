796143082211c5f6cddd7b5b2252124d585ec901
CommanderSpinner <klaus.kutulu@gmail.com>
2024-12-03 00:15:22 +0100

    Update httpreq.cpp

--- httpreq.cpp
+++ httpreq.cpp
@@ -26,32 +26,30 @@ HttpReq::~HttpReq()
 bool HttpReq::sendReq() // unfinnished
 {
     try {
-        // Initialisieren von cURLpp
+        // Initialising of cURLpp
         curlpp::Cleanup cleaner;
 
-        // URL des Formularziels
-        std::string url = "http://example.com/formular"; // Setze die Ziel-URL
 
-        // Erstellen des Requests
+        // create req
         curlpp::Easy request;
-        request.setOpt(new curlpp::options::Url(url));
+        request.setOpt(new curlpp::options::Url(this->url));
 
-        // Formulardaten festlegen
+        // set form data
         curlpp::Forms formParts;
+        //unfinnished
         formParts.push_back(new curlpp::FormParts::Content("name", "Max Mustermann"));
         formParts.push_back(new curlpp::FormParts::Content("email", "max@example.com"));
 
-        // Formular zum Request hinzufügen
         request.setOpt(new curlpp::options::HttpPost(formParts));
 
         // Anfrage ausführen
         request.perform();
 
-        std::cout << "Formular erfolgreich gesendet!" << std::endl;
+        qDebug() << "request send" << std::endl;
     } catch (curlpp::RuntimeError &e) {
-        std::cerr << "Runtime Error: " << e.what() << std::endl;
+        qDebug() << "Runtime Error: " << e.what() << std::endl;
     } catch (curlpp::LogicError &e) {
-        std::cerr << "Logic Error: " << e.what() << std::endl;
+        qDebug() << "Logic Error: " << e.what() << std::endl;
     }
 
     return true;
