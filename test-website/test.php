9ea2ffc561c62d8b298e6897d9f095acb6ae81c1
CommanderSpinner <klaus.kutulu@gmail.com>
2024-12-30 13:22:26 +0100

    finnished alternativ test site

--- test-website/test.php
+++ test-website/test.php
@@ -1,12 +1,23 @@
+<?php
+$login_name = $_POST["name"];
+$login_password = $_POST["password"];
+
+if(!empty($$login_name) && !empty($login_password){
+    if($login_name == "admin" && $login_password == "hans123"){
+        echo "login correct";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <head>
 </head>
-<body>
-<form action="test.php" method="POST">
-<label>name</label>
-<input type="text" name="name" /><br>
-<label>password</label>
-<input type="text" name="password" />
-<input type="submit" value"send">
+<body><h1>test page</h1>
+    <form action="test.php" method="POST">
+    <label>name</label>
+    <input type="text" name="name" /><br>
+    <label>password</label>
+    <input type="text" name="password" />
+    <input type="submit" value"send">
 </form>
 </body>
