467db087d1c3607aeb18251caa4604e74d397824
CommanderSpinner <klaus.kutulu@gmail.com>
2024-12-30 13:08:49 +0100

    started on alternative website to test

---
+++ test-website/test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+<form action="test.php" method="POST">
+<label>name</label>
+<input type="text" name="name" /><br>
+<label>password</label>
+<input type="text" name="password" />
+<input type="submit" value"send">
+</form>
+</body>
